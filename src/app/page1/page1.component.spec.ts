f1acefbfd5b216761338a3d34b8c393c2c8e9733
Koeco97 <koeco97@gmail.com>
2024-08-18 18:52:26 +0200

    add unit test (course part 2)

--- src/app/page1/page1.component.spec.ts
+++ src/app/page1/page1.component.spec.ts
@@ -32,4 +32,16 @@ describe('Page1Component', () => {
     expect(component.numberOfBooksWrittenByMatt).toEqual(startValue + 1);
   })
 
+  it('number of books written by matt is incremented correctly version 2', () => {
+    const startValue = component.numberOfBooksWrittenByMatt;
+    const book = new Book();
+    book.author = 'matt';
+    const dataService = new DataService();
+    component = new Page1Component(dataService);
+    component.ngOnInit();
+    dataService.addBook(book);
+    expect(component.numberOfBooksWrittenByMatt).toEqual(startValue + 1);
+  })
+
+
 });
